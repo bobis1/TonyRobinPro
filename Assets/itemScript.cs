0acff193d7b3bc19da8cf129bb9470af53b44233
bobis <josephwaizy@gmail.com>
2025-04-09 16:23:32 -0500

    Added instantiate

--- Assets/itemScript.cs
+++ Assets/itemScript.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class itemScript : MonoBehaviour
 {
     public GameObject itemBox;
+    public int numberOfBoxes;
+    public GameObject spawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < numberOfBoxes; i++)
+        {
+             GameObject itemBoxClone = Instantiate(itemBox, transform.position, transform.rotation);
+        }
     }
 
     // Update is called once per frame
