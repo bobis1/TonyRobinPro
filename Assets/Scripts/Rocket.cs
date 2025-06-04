e5c3294e3859bb9c342884633fecd67540d44060
bobis <josephwaizy@gmail.com>
2025-06-04 17:00:17 -0500

    Finished rocket and bomb obstacles

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BombFling : MonoBehaviour
+public class Rocket : MonoBehaviour
 {
+    public Vector3 impulse = new Vector3(100.0f, 300.0f, 0.0f);
+    private Rigidbody rb;
+    private ParticleSystem ps;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+        ps = GetComponent<ParticleSystem>();
+        rb.AddForce(impulse, ForceMode.Impulse);
         
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position += transform.forward * Time.deltaTime * 50;
     }
     private void OnCollisionEnter(Collision collision)
     {
+        ps.Stop();
+
         if (collision.gameObject.tag == "Obstacle")
         {
             collision.gameObject.SetActive(false);
             this.gameObject.SetActive(false);
         }
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
+
+
