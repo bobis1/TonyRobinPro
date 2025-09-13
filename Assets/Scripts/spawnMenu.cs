0199bee9aa52188d34e5c590eadbb80c9770bb23
bobis <josephwaizy@gmail.com>
2025-09-13 18:41:36 -0500

    added spawn menu

---
+++ Assets/Scripts/spawnMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnMenu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject[] Objects;
+    public GameObject player;
+
+    public bool isMenuOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SpawnMenuOpen()
+    {
+        if (isMenuOpen == false)
+        {
+            menu.SetActive(true);
+            isMenuOpen = true;
+        }
+        else if (isMenuOpen == true)
+        {
+            menu.SetActive(false);
+            isMenuOpen = false;
+        }
+    }
+    public void Barrel()
+    {
+        GameObject obj = Objects[0];
+        Instantiate(obj, player.transform.position, player.transform.rotation);
+        obj.SetActive(true);
+    }
+}
