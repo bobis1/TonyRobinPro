cee5e429f6a98b9e58a7e2e7117a565d629d6133
bobis <josephwaizy@gmail.com>
2025-10-04 11:59:28 -0500

    Work on the camera more
    https://www.youtube.com/watch?v=h7gpymxdLrQ

--- Assets/Scripts/CodeyMove.cs
+++ Assets/Scripts/CodeyMove.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 //using UnityEngine.TestTools.Constraints;
 public class CodeyMove : MonoBehaviour
 {
     public float speed;
     public float playerJump = 10f;
-    Animator anim;
     public bool running = false;
     public bool canMove = true;
     public bool isGrounded = false;
     public Vector3 move;
     public float _rotationSpeed = 50f;
     private Rigidbody rb;
+    public Transform player;
+    public float rotationSpeed = 10f;
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
         speed = 50f;
@@ -34,7 +36,7 @@ public class CodeyMove : MonoBehaviour
             transform.Rotate(rotation);
             rb.AddForce(move, ForceMode.VelocityChange);
 
-            anim.SetBool("isRunning", move != Vector3.zero);
+            //anim.SetBool("isRunning", move != Vector3.zero);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
