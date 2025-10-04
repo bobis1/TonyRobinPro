09d1b38e354f5230c0de62238a97c94667635651
bobis <josephwaizy@gmail.com>
2025-10-04 17:35:08 -0500

    fixed the camera

--- Assets/Scripts/CodeyMove.cs
+++ Assets/Scripts/CodeyMove.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Unity.VisualScripting;
+using Unity.Mathematics;
+using System.Net.NetworkInformation;
 //using UnityEngine.TestTools.Constraints;
 public class CodeyMove : MonoBehaviour
 {
@@ -15,6 +18,41 @@ public class CodeyMove : MonoBehaviour
     private Rigidbody rb;
     public Transform player;
     public float rotationSpeed = 10f;
+    public float bottomClamp;
+    public float topClamp;
+
+    private bool isCursorLocked;
+    private float cinemachineTargetPitch;
+    private float cinemachineTargetYaw;
+
+    private void LateUpdate()
+    {
+        CameraLogic();
+    }
+    private void CameraLogic()
+    {
+        float mousex = GetMouseInput("Mouse X");
+        float mousey = GetMouseInput("Mouse Y");
+
+        cinemachineTargetPitch = UpdateRotation(cinemachineTargetPitch, mousey, bottomClamp, float.MaxValue, true);
+        cinemachineTargetYaw = UpdateRotation(cinemachineTargetYaw, mousex, float.MinValue, float.MaxValue, false);
+        ApplyRotations(cinemachineTargetPitch, cinemachineTargetYaw);
+
+    }
+    private void ApplyRotations(float pitch, float yaw)
+    {
+        player.transform.rotation = Quaternion.Euler(pitch, yaw, player.eulerAngles.z);
+    }
+    private float UpdateRotation(float currentRotation, float input, float min, float max, bool isXAxis)
+    {
+        currentRotation += isXAxis ? -input : input;
+        return Mathf.Clamp(currentRotation, min, max);
+    }
+
+        private float GetMouseInput(string axis)
+    {
+        return Input.GetAxis(axis) * rotationSpeed * Time.deltaTime;
+    }
 
     void Start()
     {
@@ -43,6 +81,19 @@ public class CodeyMove : MonoBehaviour
             if (isGrounded == true)
             {
                 rb.AddForce(transform.up * playerJump, ForceMode.Impulse);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (isCursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                isCursorLocked = false;
+            }
+            else if (!isCursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                isCursorLocked = true;
             }
         }
 
