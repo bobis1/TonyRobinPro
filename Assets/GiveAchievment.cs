ae2372dfae5803f46225b657d18be7e892c43ca4
bobis <josephwaizy@gmail.com>
2025-10-11 12:02:53 -0500

    added some stuff

---
+++ Assets/GiveAchievment.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class GiveAchievment : MonoBehaviour
+{
+    private string[] names = { "Touch Box", "get 10000 score", "freeBird!!!", "go to the hole"}; 
+    Dictionary<string, bool> achivements = new Dictionary<string, bool>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < names.Length; i++) { 
+            achivements.Add(names[i], false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerPrefs.SetInt("Touch Box", achivements["Touch Box"]);
+    
+    }
+}
