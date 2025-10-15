b5dd227e4cfddcc61bad7e0df9db0293d921cc86
bobis <josephwaizy@gmail.com>
2025-10-14 21:58:23 -0500

    achievements dont work

--- Assets/AchievmentShow.cs
+++ Assets/AchievmentShow.cs
@@ -8,7 +8,7 @@ public class AchievmentShow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        touchBox.SetActive(false);
     }
 
     // Update is called once per frame
